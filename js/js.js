b34532dd4bcdd7fd59e77634eac8a275a8e61d12
Kyle <kylerobinsonmail@gmail.com>
2022-10-07 12:45:39 -0400

    working show/hide toggles

--- js/js.js
+++ js/js.js
@@ -12,15 +12,46 @@ window.onload = function() {
     const aboutSection      = document.getElementById('secAbout');
     const contactSection    = document.getElementById('secContact');
 
-    homeBtn.onclick     = function() {hideSection();showSection()};
-    musicBtn.onclick    = function() {hideSection();showSection()};
-    aboutBtn.onclick    = function() {hideSection();showSection()};
-    contactBtn.onclick  = function() {hideSection();showSection()};
+    // Set section visibility for first time load
+    homeSection.style.visibility    = 'visible';
+    musicSection.style.visibility   = 'hidden';
+    aboutSection.style.visibility   = 'hidden';
+    contactSection.style.visibility = 'hidden';
 
-    function hideSection() {
-        homeSection.style.visibility = 'visible';
-    }
+    //homeSection.style.visibility = 'visible';
 
-    function showSection() {
+    homeBtn.onclick     = function() {toggleSectionVisibility('home');};
+    musicBtn.onclick    = function() {toggleSectionVisibility('music');};
+    aboutBtn.onclick    = function() {toggleSectionVisibility('about');};
+    contactBtn.onclick  = function() {toggleSectionVisibility('contact');};
+
+    // There's probably a better way to do this lol
+    function toggleSectionVisibility(clicked) {
+        switch(clicked) {
+            case 'home':
+                homeSection.style.visibility    = 'visible';
+                musicSection.style.visibility   = 'hidden';
+                aboutSection.style.visibility   = 'hidden';
+                contactSection.style.visibility = 'hidden';
+                break;
+            case 'music':
+                homeSection.style.visibility    = 'hidden';
+                musicSection.style.visibility   = 'visible';
+                aboutSection.style.visibility   = 'hidden';
+                contactSection.style.visibility = 'hidden';
+                break;
+            case 'about':
+                homeSection.style.visibility    = 'hidden';
+                musicSection.style.visibility   = 'hidden';
+                aboutSection.style.visibility   = 'visible';
+                contactSection.style.visibility = 'hidden';
+                break;
+            case 'contact':
+                homeSection.style.visibility    = 'hidden';
+                musicSection.style.visibility   = 'hidden';
+                aboutSection.style.visibility   = 'hidden';
+                contactSection.style.visibility = 'visible';
+                break;
+        }
     }
 }
