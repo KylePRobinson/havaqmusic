528c0d89c0b1b372d35da613d1930f1c49f93ee5
Kyle <kylerobinsonmail@gmail.com>
2022-10-22 01:28:14 -0400

    css version

--- js/head.js
+++ js/head.js
@@ -4,17 +4,17 @@ document.write(`
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Electronic & Metal Music">
-        <link rel='stylesheet' type='text/css' href='css/animations.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/css.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/footer.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/nav.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/navmini.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/home.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/music.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/about.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/contact.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/queries.css?v=1'>
-        <link rel='stylesheet' type='text/css' href='css/sections.css?v=1'>
+        <link rel='stylesheet' type='text/css' href='css/animations.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/css.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/footer.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/nav.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/navmini.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/home.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/music.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/about.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/contact.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/queries.css?v=2'>
+        <link rel='stylesheet' type='text/css' href='css/sections.css?v=2'>
         <script type="text/javascript" src="js/js.js"></script>
     </head>
 `)
